08b0d63e786dc19c3c01d253a6547510ad657bf3
Alan Sokurov <sokalcc@gmail.com>
2023-02-05 02:37:02 +0300

    modal component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <script setup></script>
 
 <template>
-	<div class="container mx-auto p-2.5 sm:py-5 sm:px-0">
-		<base-button label="Заказать в Москву" class="mb-2.5 sm:mb-0 sm:mr-5" />
-		<base-button label="Заказать в Санкт-Петербург" color="teal-600" />
-	</div>
+	<main class="container mx-auto p-2.5 sm:py-5 sm:px-0">
+		<h1 class="sr-only">Заказать</h1>
+
+		<base-button
+			label="Заказать в Москву"
+			class="mb-2.5 sm:mb-0 sm:mr-5"
+			@click="modalVisible = true"
+		/>
+		<base-button
+			label="Заказать в Санкт-Петербург"
+			color="teal-600"
+			@click="modalVisible = true"
+		/>
+
+		<base-modal
+			title="Заказать звонок"
+			:show="modalVisible"
+			@close-modal="modalVisible = false"
+		>
+			<div>...</div>
+		</base-modal>
+	</main>
 </template>
 
-<style scoped></style>
 <script>
 	import BaseButton from './components/BaseButton.vue';
+	import BaseModal from './components/BaseModal.vue';
 	export default {
-		components: { BaseButton },
+		components: { BaseButton, BaseModal },
+
+		data() {
+			return {
+				modalVisible: false,
+			};
+		},
 	};
 </script>
+
+<style scoped></style>
---
+++ src/components/BaseModal.vue
@@ -0,0 +1,44 @@
+<template>
+	<Teleport to="#modals">
+		<div
+			v-if="show"
+			class="fixed inset-0 flex items-center justify-center bg-gray-500 p-2.5 opacity-75 sm:p-10"
+			@click="$emit('close-modal')"
+		>
+			<div
+				class="w-full rounded-lg bg-white p-6 font-medium shadow-xl lg:w-1/2"
+				@click.stop
+			>
+				<h2
+					class="mb-9 text-base leading-none text-gray-900 sm:mb-7 md:mb-6"
+				>
+					{{ title }}
+				</h2>
+
+				<slot></slot>
+			</div>
+		</div>
+	</Teleport>
+</template>
+
+<script>
+	export default {
+		emits: ['close-modal'],
+
+		name: 'base-modal',
+
+		props: {
+			show: {
+				type: Boolean,
+				default: false,
+			},
+
+			title: {
+				type: String,
+				required: true,
+			},
+		},
+	};
+</script>
+
+<style scoped></style>
